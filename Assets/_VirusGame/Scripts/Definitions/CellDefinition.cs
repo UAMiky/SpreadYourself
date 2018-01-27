29d1e738584e97459d734b28cd78cd28c1703c10
Miguel Company <m.company.company@gmail.com>
2018-01-27 11:23:45 +0100

    Definición de célula

---
+++ Assets/_VirusGame/Scripts/Definitions/CellDefinition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct SpawnConfig
+{
+    [Tooltip("Time to wait for the first spawn")]
+    public float firstSpawnTime;
+
+    [Tooltip("Time to wait for the second and subsequent spawns")]
+    public float nextSpawnTime;
+
+    [Tooltip("Total number of items to spawn")]
+    public int maxSpawnNumber;
+}
+
+[CreateAssetMenu]
+public class CellDefinition : ScriptableObject
+{
+    public SpawnConfig enemiesSpawnConfig;
+    public SpawnConfig clonesSpawnConfig;
+}
