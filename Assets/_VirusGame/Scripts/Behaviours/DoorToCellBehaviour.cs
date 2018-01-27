32b6c1d012359c7c9b4d7f3347d364d850bda120
Miguel Company <m.company.company@gmail.com>
2018-01-27 21:20:14 +0100

    Puertas de células permiten interacción por trigger

--- Assets/_VirusGame/Scripts/Behaviours/DoorToCellBehaviour.cs
+++ Assets/_VirusGame/Scripts/Behaviours/DoorToCellBehaviour.cs
@@ -8,18 +8,41 @@ public class DoorToCellBehaviour : MonoBehaviour
 {
     public CellDefinition celula;
 
+    public bool triggerEnabled = false;
+    public bool clickEnabled = true;
+
     private Transform player;
 
 
     void Start()
     {
-        GetComponent<VRInteractiveItem>().OnClick += OnVRClick;
+        if(clickEnabled)
+            GetComponent<VRInteractiveItem>().OnClick += OnVRClick;
+
         player = this.FindWithTag<Transform>("Player").transform;
+    }
+
+    private void OnTriggerEnter (Collider other)
+    {
+        if (!this.enabled)
+            return;
+
+        if(triggerEnabled)
+            CellBehaviour.Instance.PlayerEntered(player, celula);
+
+        if(clickEnabled)
+            GetComponent<VRInteractiveItem>().OnClick -= OnVRClick;
+
+        this.enabled = false;
     }
 
     private void OnVRClick()
     {
+        if (!this.enabled)
+            return;
+
         GetComponent<VRInteractiveItem>().OnClick -= OnVRClick;
         CellBehaviour.Instance.PlayerEntered(player, celula);
+        this.enabled = false;
     }
 }
