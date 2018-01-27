15b4d5e6780a06041851527cdbe80227295d5ab3
Miguel Company <m.company.company@gmail.com>
2018-01-27 20:07:44 +0100

    FindWithTag genérico

---
+++ Assets/_VirusGame/Scripts/MonoBehaviourExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MonoBehaviourExtensions
+{
+    public static T FindWithTag<T> (this MonoBehaviour behaviour, string tag) where T : class
+    {
+        var go = GameObject.FindGameObjectWithTag(tag);
+        return go ? go.GetComponent<T>() : null;
+    }
+}
