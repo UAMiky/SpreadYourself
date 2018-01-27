17e7e47fb871549ae974bdb7fd04009791cad2be
EDSON\Edson <edson.linares1@gmail.com>
2018-01-27 04:25:08 -0500

    Agregado un Script inservible por el momento xD

---
+++ Assets/_VirusGame/Scripts/ControllPoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControllPoint : MonoBehaviour {
+
+    [SerializeField] Renderer rend;
+    [SerializeField] Image filled;
+    public float time = 6f;
+
+    private void OnTriggerStay(Collider other)
+    { 
+    }
+}
